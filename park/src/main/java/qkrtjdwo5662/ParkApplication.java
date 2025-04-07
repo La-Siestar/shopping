93d102a81be7eb71796c491e2e1cb4096c0b7868
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2025-04-07 13:42:54 +0900

    #1 feat: project init

---
+++ park/src/main/java/qkrtjdwo5662/ParkApplication.java
@@ -0,0 +1,11 @@
+package qkrtjdwo5662;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ParkApplication.class, args);
+    }
+}
